1ab84545cf9d766d5bcecd5dbc5abc51c5dbed0d
xd11223344778899-oss <xd11223344778899@gmail.com>
2026-01-13 16:04:38 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -4,7 +4,7 @@ export default [
         channelId: "1356340858290442264",
         serverId: "1300245281987629147",
         token: process.env.token1,
-        selfDeaf: true,
+        selfDeaf: false,
         autoReconnect: {
             enabled: true,
             delay: 5, // ثواني

b50523761529b428702c1c435028cbbb95f1e154
xd11223344778899-oss <xd11223344778899@gmail.com>
2026-01-13 22:31:15 +0300

    Add autoReconnect configuration to tokens

--- tokens.js
+++ tokens.js
@@ -5,6 +5,7 @@ export default [
         serverId: "1300245281987629147",
         token: process.env.token1,
         selfDeaf: false,
+   autoReconnect: {
             enabled: true,
             delay: 5, // ثواني
             maxRetries: 5,
@@ -18,6 +19,7 @@ export default [
         channelId: "1356340778674290779",
         serverId: "1300245281987629147",
         token: process.env.token2,
+            autoReconnect: {
         selfDeaf: false,
             enabled: true,
             delay: 5, // ثواني
@@ -32,6 +34,7 @@ export default [
         channelId: "1356340816381083839",
         serverId: "1300245281987629147",
         token: process.env.token3,
+            autoReconnect: {
         selfDeaf: false,
             enabled: true,
             delay: 5, // ثواني
@@ -46,6 +49,7 @@ export default [
         channelId: "1356340823658332301",
         serverId: "1300245281987629147",
         token: process.env.token4,
+            autoReconnect: {
         selfDeaf: false,
             enabled: true,
             delay: 5, // ثواني
@@ -60,6 +64,7 @@ export default [
         channelId: "1356340827907035156",
         serverId: "1300245281987629147",
         token: process.env.token5,
+             autoReconnect: {
         selfDeaf: true,
             enabled: true,
             delay: 5, // ثواني
@@ -74,6 +79,7 @@ export default [
         channelId: "1356340832227164221",
         serverId: "1300245281987629147",
         token: process.env.token6,
+            autoReconnect: {
         selfDeaf: false,
             enabled: true,
             delay: 5, // ثواني
@@ -88,6 +94,7 @@ export default [
         channelId: "1356340836425662565",
         serverId: "1300245281987629147",
         token: process.env.token7,
+             autoReconnect: {
         selfDeaf: false,
             enabled: true,
             delay: 5, // ثواني
@@ -102,6 +109,7 @@ export default [
         channelId: "1356340840376566032",
         serverId: "1300245281987629147",
         token: process.env.token8,
+             autoReconnect: {
         selfDeaf: false,
             enabled: true,
             delay: 5, // ثواني
@@ -116,6 +124,7 @@ export default [
         channelId: "1356340849151180800",
         serverId: "1300245281987629147",
         token: process.env.token9,
+             autoReconnect: {
         selfDeaf: false,
             enabled: true,
             delay: 5, // ثواني
@@ -130,6 +139,7 @@ export default [
         channelId: "1356340858290442264",
         serverId: "1300245281987629147",
         token: process.env.token10,
+             autoReconnect: {
         selfDeaf: true,
             enabled: false,
             delay: 5, // ثواني
@@ -144,6 +154,7 @@ export default [
         channelId: "1448856690073272393",
         serverId: "1300245281987629147",
         token: process.env.token11,
+             autoReconnect: {
         selfDeaf: false,
             enabled: true,
             delay: 5, // ثواني
@@ -158,6 +169,7 @@ export default [
         channelId: "1356341154689581136",
         serverId: "1300245281987629147",
         token: process.env.token12,
+             autoReconnect: {
         selfDeaf: true,
             enabled: true,
             delay: 5, // ثواني
@@ -172,6 +184,7 @@ export default [
         channelId: "1356341154689581136",
         serverId: "1300245281987629147",
         token: process.env.token13,
+             autoReconnect: {
         selfDeaf: true,
             enabled: true,
             delay: 5, // ثواني
@@ -186,6 +199,7 @@ export default [
         channelId: "1356341154689581136",
         serverId: "1300245281987629147",
         token: process.env.token14,
+             autoReconnect: {
         selfDeaf: true,
             enabled: true,
             delay: 5, // ثواني
@@ -200,6 +214,7 @@ export default [
         channelId: "1356341154689581136",
         serverId: "1300245281987629147",
         token: process.env.token15,
+             autoReconnect: {
         selfDeaf: true,
             enabled: true,
             delay: 5, // ثواني
@@ -214,6 +229,7 @@ export default [
         channelId: "1356341203666337844",
         serverId: "1300245281987629147",
         token: process.env.token16,
+             autoReconnect: {
         selfDeaf: true,
             enabled: true,
             delay: 5, // ثواني

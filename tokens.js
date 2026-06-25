c8d3946ec2e5811dd9191410a617f19ca0bd4815
xd11223344778899-oss <xd11223344778899@gmail.com>
2026-06-25 18:30:51 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -31,7 +31,7 @@ export default [
         selfMute: False,
     },
         {
-        channelId: "1356340827907035156",
+        channelId: "1508321243412299807",
         serverId: "1300245281987629147",
         token: process.env.token3,
         selfDeaf: true,
@@ -56,12 +56,12 @@ export default [
             maxRetries: 5,
         },
         presence: {
-            status: "idle",
+            status: "dnd",
         },
         selfMute: true,
     },
         {
-        channelId: "1356340858290442264",
+        channelId: "1508317583311634544",
         serverId: "1300245281987629147",
         token: process.env.token5,
         selfDeaf: false,
@@ -73,10 +73,10 @@ export default [
         presence: {
             status: "dnd",
         },
-        selfMute: true,
+        selfMute: false,
     },
         {
-        channelId: "1356341203666337844",
+        channelId: "1508254884472815656",
         serverId: "1300245281987629147",
         token: process.env.token6,
         selfDeaf: false,
@@ -121,7 +121,7 @@ export default [
         selfMute: false,
     },
         {
-        channelId: "1356341203666337844",
+        channelId: "1508317752065134612",
         serverId: "1300245281987629147",
         token: process.env.token9,
         selfDeaf: false,
@@ -133,10 +133,10 @@ export default [
         presence: {
             status: "dnd",
         },
-        selfMute: false,
+        selfMute: true,
     },
         {
-        channelId: "1356341203666337844",
+        channelId: "1508317752065134612",
         serverId: "1300245281987629147",
         token: process.env.token10,
         selfDeaf: false,
